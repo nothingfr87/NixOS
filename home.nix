aac1f900bd6acf6c83ae0bb8d82968fa49c48b21
Nothing <173809041+nothingfr87@users.noreply.github.com>
2026-01-19 13:05:14 +0200

    Fixed a problem

--- home.nix
+++ home.nix
@@ -48,8 +48,8 @@
     };
   };
  
-  home.file.".config/nvim".source = ./config/nvim;
-  home.file.".config/i3".source = ./config/i3;
-  home.file.".config/i3status".source = ./config/i3status;
-  home.file.".config/alacritty".source = ./config/alacritty;
+  home.file.".config/nvim".source = lib.file.mkOutOfStoreSymlink "./config/nvim";
+  home.file.".config/i3".source = lib.file.mkOutOfStoreSymlink "./config/i3";
+  home.file.".config/i3status".source = lib.file.mkOutOfStoreSymlink "./config/i3status";
+  home.file.".config/alacritty".source = lib.file.mkOutOfStoreSymlink "./config/alacritty";
 }
